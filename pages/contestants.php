8b494ebb03207cc4f50023eccb527816ee589789
nicky <nikhi170104030@iitg.ac.in>
2019-03-23 12:44:09 +0530

    Edit 2019

---
+++ pages/contestants.php
@@ -0,0 +1,425 @@
+		 <!---728x90--->
+         <!-- footer-top -->
+	<div class="footer-top">
+		<div class="container">
+			<div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+				<br><br>
+			<h3>Contestants - Gymkhana Election 2018</h3>
+			<p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">Vice-President</p>
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/VP_Anmol.pdf')">
+					<img src="Pictures/VP_Anmol.PNG" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Anmol  <span>deep</span></h2>
+						<p>160103011</p>
+						<span class="badge badge-success">View Agenda</span>
+					</figcaption>
+				</figure>
+						<!--figure class="effect-winston" onclick="location.reload();location.href='?gym_election=shivam_kumar'">-->
+
+
+					<figure class="effect-winston" onclick="window.open('./downloads/agenda/VP_MohitMishra.pdf')">
+				<img src="Pictures/VP_Mohit.PNG" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Mohith<span>Mishra</span></h2>
+					<p>156302008</p>
+					<span class="badge badge-success">View Agenda</span>
+				</figcaption>
+			 </figure>
+
+			 <figure class="effect-winston" onclick="window.open('./downloads/agenda/VP_Vikrant.pdf')">
+			<img src="Pictures/VP_Vikrant.jpg" alt="" class="img-responsive one" />
+			<figcaption>
+				<h2>Vikrant <span>Singh</span></h2>
+				<p>166102108</p>
+				<span class="badge badge-success">View Agenda</span>
+			</figcaption>
+		</figure>
+
+			</div>
+			<div class="clearfix"> </div>
+			<div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+			<p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">General Secretary, Cultural Board</p>
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/GSCult_AdityaKakoti.pdf')">
+					<img src="Pictures/GSCult_Aditya.jpg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Aditya <span> Kakoti</span></h2>
+						<p>170104005</p>
+						<span class="badge badge-success">View Agenda</span>
+					</figcaption>
+
+			</div>
+			<div class="clearfix"> </div>
+			<div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+			<p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">General Secretary, Hostel Affairs Board</p>
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/GSHAB_Himanchal.pdf')">
+					<img src="Pictures/HAB_ HimanchalSingh.jpg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Himanchal <span> Singh </span></h2>
+						<p>176102109</p>
+						<span class="badge badge-success">View Agenda</span>
+					</figcaption>
+				</figure>
+
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/GSHAB_Vikash.pdf')">
+					<img src="Pictures/HAB_Vikash.jpg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Vikas  <span> Dahiya </span></h2>
+						<p>184103222</p>
+						<span class="badge badge-success">View Agenda</span>
+					</figcaption>
+				</figure>
+
+				
+
+
+			</div>
+			<div class="clearfix"> </div>
+			<div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+			<p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">General Secretary, Sports Board</p>
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/GSSports_ArvindPatidar.pdf')">
+				<img src="Pictures/Sports_Arvind.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2> Aravind <span> Patidar </span></h2>
+					<p> 170122005</p>
+					<span class="badge badge-success">View Agenda</span>
+				</figcaption>
+			</figure>
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/GSSports_Neeraj.pdf')">
+				<img src="Pictures/Sports_Neeraj.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2> Neeraj <span> Kumar Rathore </span></h2>
+					<p>170107042</p>
+					<span class="badge badge-success">View Agenda</span>
+				</figcaption>
+			</figure>
+
+			</div>
+
+			<div class="clearfix"> </div>
+			<div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+			<p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">General Secretary, Technical Board</p>
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/GSTech_Shrey.pdf')">
+				<img src="Pictures/GS_TechBoard_ShreyJain.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2> Shrey <span> Jain </span></h2>
+					<p>......</p>
+					<span class="badge badge-success">View Agenda</span>
+				</figcaption>
+			</figure>
+
+			
+
+
+
+			</div>
+			<div class="clearfix"> </div>
+			<div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+			<p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">General Secretary, Welfare Board</p>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/GSWelfare_Aditya.pdf')">
+				<img src="Pictures/GS_Welfare_Aaditya_Sanwal.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2> Aaditya <span> Sanwal </span></h2>
+					<p>160102001</p>
+					<span class="badge badge-success">View Agenda</span>
+				</figcaption>
+			</figure>
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/GSWelfare_Namrata.pdf')">
+				<img src="Pictures/WelfareSecretary_Namrata_Gupta.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2> Namrata <span>Gupta </span></h2>
+					<p>160106027</p>
+					<span class="badge badge-success">View Agenda</span>
+				</figcaption>
+			</figure>
+			
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/GSWelfare_Purshottam.pdf')">
+				<img src="Pictures/Welfare_Purushottam.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2> Purshottam<span> Gupta </span></h2>
+					<p>184103112</p>
+					<span class="badge badge-success">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+
+			</div>
+
+			<div class="clearfix"> </div>
+ 		 <div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+ 		 <p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">General Secretary, Students' Web Committee</p>
+
+
+ 		 <figure class="effect-winston" onclick="window.open('./downloads/agenda/GSSWC_GoddamNikhil.pdf')">
+ 			 <img src="Pictures/GenSecSWC_Gaddam_Nikhil.jpg" alt="" class="img-responsive one" />
+ 			 <figcaption>
+ 				 <h2> Nikhil <span> Gaddam </span></h2>
+ 				 <p> 170104030</p>
+ 				 <span class="badge badge-success">View Agenda</span>
+ 			 </figcaption>
+ 		 </figure>
+
+ 		 </div>
+
+		 <div class="clearfix"> </div>
+			<div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+			<p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">General Secretary, SAIL</p>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/GSSAIL_Mayank.pdf')">
+				<img src="Pictures/GS_SAIL_Mayank_Singla.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2> Mayank <span> Singla </span></h2>
+					<p> 160102040</p>
+					<span class="badge badge-success">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+			
+			</div>
+
+
+			<div class="clearfix"> </div>
+			<div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+			<p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">Girls Senators</p>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/GirlSenator_Nishtha.pdf')">
+				<img src="Pictures/GirlsSenator_Nishtha_Rautela.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Nishitha <span>Routela</span></h2>
+					<p>170102045</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/GirlSenator_Simran.pdf')">
+				<img src="Pictures/GIRLSENATOR_SIMRANLOKWANI.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Simran<span> Lokwani</span></h2>
+					<p>170122044</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+						
+
+			</div>
+
+
+
+      <!-- UG Senators -->
+			<div class="clearfix"> </div>
+			<div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+			<p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">UG Senators</p>
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/UGEnator_VikasPatel.pdf')">
+					<img src="Pictures/UGSENATOR_VIKAS_PATEL.jpg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Vikas <span> Patel</span></h2>
+						<p>170106062</p>
+						<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+					</figcaption>
+				</figure>
+
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/UGSenator_Akash.pdf')">
+					<img src="Pictures/UGSenator_Akash_Gupta.jpg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Akash <span> Gupta </span></h2>
+						<p>170104010</p>
+						<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+					</figcaption>
+				</figure>
+
+
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/UGSenator_Anirudh.pdf')">
+					<img src="Pictures/UGSENATOR_ANIRUDH_PRATAP_SINGH.jpeg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Anirudh <span> Pratap Singh </span></h2>
+						<p>170103007</p>
+						<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+					</figcaption>
+				</figure>
+
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/UGSenator_KShriAchla.pdf')">
+					<img src="Pictures/UG_Achala.jpg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Sri Achala<span> K </span></h2>
+						<p>170106029</p>
+						<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+					</figcaption>
+				</figure>
+
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/UGSenator_Manish.pdf')">
+					<img src="Pictures/UG_SENATOR_MANISH_ANAND.jpg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Manish <span>  Anand</span></h2>
+						<p>170107032</p>
+						<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+					</figcaption>
+				</figure>
+
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/UGSenator_Rahul.pdf')">
+					<img src="Pictures/UG_Senator_Rahul.jpg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Rahul<span>  Meel</span></h2>
+						<p>170103051</p>
+						<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+					</figcaption>
+				</figure>
+
+
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/UGSenator_Rudranshu.pdf')">
+					<img src="Pictures/UG Senator_Rudranshu Prajapati.jpg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Rudranshu <span>  Prajapati</span></h2>
+						<p>170108032</p>
+						<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+					</figcaption>
+				</figure>
+
+				<figure class="effect-winston" onclick="window.open('./downloads/agenda/UGSenator_Sarth.pdf')">
+					<img src="Pictures/UGsenator_Sarth VItekar.jpg" alt="" class="img-responsive one" />
+					<figcaption>
+						<h2>Sarth<span>  Vitekar</span></h2>
+						<p>170103060</p>
+						<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+					</figcaption>
+				</figure>
+
+				
+
+
+			</div>
+
+
+
+
+			<div class="clearfix"> </div>
+			<div class="footer-grids wlcome-grid-left" style="margin-top:5%;">
+			<p style="color:#df1728;font-size:20px;margin-top: 4%;padding-left: 1%;">PG Senators</p>
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_Atul.pdf')">
+				<img src="Pictures/PG_senator_Atul_Kumar_bhardwaj.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Atul <span>  Kumar Bharadwah</span></h2>
+					<p>184103306</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_AvinashPaul.pdf')">
+				<img src="Pictures/PGSenator_Avinash_Paul.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Avinash <span>  Paul</span></h2>
+					<p>184102301</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_Ayushman.pdf')">
+				<img src="Pictures/PG senator_Ayushman singh.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Ayushman Singh <span>  Bhadouriya</span></h2>
+					<p>184103204</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_Etisha.pdf')">
+				<img src="Pictures/PG Senator_Etisha.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Etisha <span>  Garg</span></h2>
+					<p>182123018</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_Keshav.pdf')">
+				<img src="Pictures/PG Senator_Keshav Kumar.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Keshav	 <span>  Kumar</span></h2>
+					<p>176107017</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_MakwanaAvinash.pdf')">
+				<img src="Pictures/PGSenator_Avinash_Makwana.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Makwana <span>  Avinash</span></h2>
+					<p>184102106</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_Mansi.pdf')">
+				<img src="Pictures/PG Senator_Mansi Saini.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Mansi <span>  Saina</span></h2>
+					<p>182123027</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_Piyush.pdf')">
+				<img src="Pictures/PGSenator_Piyush_Agnihotri.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Piysh <span>  Agnihotri</span></h2>
+					<p>184102304</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_Saksham.pdf')">
+				<img src="Pictures/PG Senator_SAKSHAM_JANGIR.JPG" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Saksham <span>  Jangir</span></h2>
+					<p>184103214</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_Sreekant.pdf')">
+				<img src="Pictures/PG Senator_G Sreekanth Yadav.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Srikanth <span>  Yadav</span></h2>
+					<p>184104106</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+			<figure class="effect-winston" onclick="window.open('./downloads/agenda/PGSenator_Vivek.pdf')">
+				<img src="Pictures/PGSenator_Vivek_Singh_Chouhan.jpg" alt="" class="img-responsive one" />
+				<figcaption>
+					<h2>Vivek <span>  Singh Chouhan</span></h2>
+					<p>184102316</p>
+					<span class="badge badge-success" style="margin-top: 5%;">View Agenda</span>
+				</figcaption>
+			</figure>
+
+
+						
+
+
+
+			</div>
+		</div>
+	</div>
+<!-- /footer-top -->
---
+++ pages/election_commission.php
@@ -0,0 +1,79 @@
+		 <!---728x90--->
+         <!-- body_grids -->
+         <div class="wlcome" style="margin-top:5%;">
+            <div class="container">
+               <div class="wlcome-grids">
+                  <div class="col-md-12 wlcome-grid-left">
+                     <div class="election_grid">
+                        <h3>The Team - Gymkhana Election 2019</h3>
+                        <div class="wlcome-grid-left-grid" style="margin-top:5%">
+                           <div class="col-xs-12 wlcome-grid-left-grid" style="padding-left:0%">
+                            <div class="about-grids">
+
+
+
+
+              <div class="col-md-4 about-grid">
+                <div class="about-grid1">
+                  <figure class="thumb">
+                    <img src="images/shobit.jpg" alt=" " class="img-responsive" />
+                    <figcaption class="caption">
+                      <h3><a href="#">Shobhit Tayal</a></h3>
+                      <span>Election Officer 1 (EO_1)</span>
+                      <p style="text-align:center; color:white;"> Phone: +91-9560455707</p>
+                      <ul>
+                        <li><a target="_blank" href="https://www.facebook.com/shobhit.tayal.9" class="f2"></a></li>
+                      </ul>
+                    </figcaption>
+                  </figure>
+                </div>
+              </div>
+
+
+
+              <div class="col-md-4 about-grid">
+								<div class="about-grid1">
+									<figure class="thumb">
+										<img src="images/fds.jpg" alt=" " class="img-responsive" />
+										<figcaption class="caption">
+											<h3><a href="#"> Urmil Dave </a></h3>
+											<span>Chief Election Officer (CEO)</span>
+											<p style="text-align:center; color:white;"> Webmail:elections@iitg.ac.in
+<br>Phone: +91-9435662359</p>
+											<ul>
+												<li><a target="_blank" href="https://www.facebook.com/urmil.dave1" class="f2"></a></li>
+											</ul>
+										</figcaption>
+									</figure>
+								</div>
+							</div>
+
+              <div class="col-md-4 about-grid">
+                <div class="about-grid1">
+                  <figure class="thumb">
+                    <img src="images/satyam.jpg" alt=" " class="img-responsive" />
+                    <figcaption class="caption">
+                      <h3><a href="#">Satyam Singh</a></h3>
+                      <span>Election Officer 2 (EO_2)</span>
+                      <p style="text-align:center; color:white;"> Phone: +91-9954249003</p>
+                      <ul>
+                        <li><a target="_blank" href="https://www.facebook.com/profile.php?id=100001662045117" class="f2"></a></li>
+                      </ul>
+                    </figcaption>
+                  </figure>
+                </div>
+              </div>
+
+
+              <div class="clearfix"> </div>
+            </div>
+                           </div>
+                           <div class="clearfix"> </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="clearfix"> </div>
+               </div>
+            </div>
+         </div>
+         <!-- //body_grids -->
